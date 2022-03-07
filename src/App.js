da30ce05298c8d15a18059e55351de3a7fe3726a
m-ghiringhelli <mghiring@gmail.com>
2022-03-07 14:57:40 -0800

    oops forgot to ACP 'cause I was too busy trying to figure out how this works

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
 
+import background from './background.png';
+
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `url(${background})` }}>
       <Header />
       <Main />
       <Footer />
--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import './Animal.css';
 
-export default function Animal() {
-  return <></>;
+export default function Animal(props) {
+  console.log(props);
+  return <div className="animal" style={{ top: props.top, left: props.left }}>
+    <img alt={`${props.name}`} src={`./animals/${props.typ}.svg`}></img>
+    <span className="name">{`${props.name}`}</span>
+    <span>{`${props.says}`}</span>
+  </div>;
 }
